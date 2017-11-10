66476cdc86485fc2aaaec7893049a40847eb78ac
RvkACEaMlh FcJVVeAncA <automate11@wavemaker.com>
2017-11-10 08:42:23 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,14 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "Auto_ZVePfwHtjD",
+  "homePage" : "Main",
+  "name" : "Auto_ZVePfwHtjD",
+  "platformType" : "WEB",
+  "securityEnabled" : "false",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
